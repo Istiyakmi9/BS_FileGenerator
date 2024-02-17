cfc65c80c18c2271b3f79e10ef362d526fc3598d
istiyakmi9 <istiyaq.mi9@gmail.com>
2024-02-17 11:17:07 +0530

    loaded library manaually

--- BS_FileGenerator/Program.cs
+++ BS_FileGenerator/Program.cs
@@ -1,81 +1,10 @@
-
-using Bot.CoreBottomHalf.CommonModal;
-using Bot.CoreBottomHalf.CommonModal.Enums;
-using BS_FileGenerator.IService;
+using BS_FileGenerator;
 using BS_FileGenerator.Middleware;
-using BS_FileGenerator.Models;
-using BS_FileGenerator.Service;
-using Confluent.Kafka;
-using DinkToPdf;
-using DinkToPdf.Contracts;
-using DocMaker.HtmlToDocx;
-using Microsoft.Extensions.Options;
-using ModalLayer;
-using Newtonsoft.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
-    .AddEnvironmentVariables();
-builder.Services.AddSingleton<ApplicationConfiguration>();
-
-var kafkaServerDetail = new ProducerConfig();
-builder.Configuration.Bind("KafkaServerDetail", kafkaServerDetail);
-builder.Services.Configure<KafkaServiceConfig>(x => builder.Configuration.GetSection(nameof(KafkaServiceConfig)).Bind(x));
-builder.Services.Configure<JwtSetting>(o => builder.Configuration.GetSection(nameof(JwtSetting)).Bind(o));
-
-builder.Services.AddSingleton<ProducerConfig>(kafkaServerDetail);
-builder.Services.AddSingleton<KafkaNotificationService>(x =>
-{
-    return new KafkaNotificationService(
-        x.GetRequiredService<IOptions<KafkaServiceConfig>>(),
-        x.GetRequiredService<ProducerConfig>(),
-        x.GetRequiredService<ILogger<KafkaNotificationService>>(),
-        builder.Environment.EnvironmentName == nameof(DefinedEnvironments.Development) ?
-                        DefinedEnvironments.Development :
-                        DefinedEnvironments.Production
-    );
-});
-
-builder.Services.AddScoped<CurrentSession>(x =>
-{
-    return new CurrentSession
-    {
-        Environment = builder.Environment.EnvironmentName == nameof(DefinedEnvironments.Development) ?
-                        DefinedEnvironments.Development :
-                        DefinedEnvironments.Production
-    };
-});
-
-builder.Services.AddControllers().AddNewtonsoftJson(options =>
-{
-    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-    options.SerializerSettings.ContractResolver = new DefaultContractResolver();
-});
-builder.Services.AddScoped<IHtmlToPdfConverter, HtmlToPdfConverter>();
-builder.Services.AddScoped<IHtmlToDocxConverter, HtmlToDocxConverter>();
-builder.Services.AddScoped<IDataTableToExcel, DataTableToExcel>();
-builder.Services.AddScoped<IDataTableToExcel, DataTableToExcel>();
-builder.Services.AddSingleton(typeof(IConverter), new SynchronizedConverter(new PdfTools()));
-builder.Services.AddScoped<IHTMLConverter, HTMLConverter>();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("bottomhalf-cors", policy =>
-    {
-        policy.AllowAnyOrigin()
-        .AllowAnyHeader()
-        .AllowAnyMethod()
-        .WithExposedHeaders("Authorization");
-    });
-});
+var startup = new Startup(builder);
+startup.ConfiguraServices();
 
 var app = builder.Build();
 
---
+++ BS_FileGenerator/Startup.cs
@@ -0,0 +1,114 @@
+﻿using Bot.CoreBottomHalf.CommonModal;
+using Bot.CoreBottomHalf.CommonModal.Enums;
+using BS_FileGenerator.IService;
+using BS_FileGenerator.Models;
+using BS_FileGenerator.Service;
+using Confluent.Kafka;
+using DinkToPdf;
+using DinkToPdf.Contracts;
+using DocMaker.HtmlToDocx;
+using Microsoft.Extensions.Options;
+using ModalLayer;
+using Newtonsoft.Json.Serialization;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.Loader;
+
+namespace BS_FileGenerator
+{
+    public class Startup
+    {
+        private readonly IConfigurationBuilder _configurationBuilder;
+        private readonly IConfiguration _configuration;
+        private readonly IServiceCollection _services;
+        private readonly IWebHostEnvironment _environment;
+
+        public Startup(WebApplicationBuilder builder)
+        {
+            _services = builder.Services;
+            _configuration = builder.Configuration;
+            _environment = builder.Environment;
+            _configurationBuilder = builder.Configuration;
+        }
+
+        public void ConfiguraServices()
+        {
+            var context = new CustomAssemblyLoadContext();            
+            context.LoadUnmanagedLibrary("libwkhtmltox");
+
+
+            _services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            _services.AddEndpointsApiExplorer();
+            _services.AddSwaggerGen();
+            _configurationBuilder.SetBasePath(_environment.ContentRootPath)
+                .AddJsonFile($"appsettings.{_environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            _services.AddSingleton<ApplicationConfiguration>();
+
+            var kafkaServerDetail = new ProducerConfig();
+            _configuration.Bind("KafkaServerDetail", kafkaServerDetail);
+            _services.Configure<KafkaServiceConfig>(x => _configuration.GetSection(nameof(KafkaServiceConfig)).Bind(x));
+            _services.Configure<JwtSetting>(o => _configuration.GetSection(nameof(JwtSetting)).Bind(o));
+
+            _services.AddSingleton<ProducerConfig>(kafkaServerDetail);
+            _services.AddSingleton<KafkaNotificationService>(x =>
+            {
+                return new KafkaNotificationService(
+                    x.GetRequiredService<IOptions<KafkaServiceConfig>>(),
+                    x.GetRequiredService<ProducerConfig>(),
+                    x.GetRequiredService<ILogger<KafkaNotificationService>>(),
+                    _environment.EnvironmentName == nameof(DefinedEnvironments.Development) ?
+                                    DefinedEnvironments.Development :
+                                    DefinedEnvironments.Production
+                );
+            });
+
+            _services.AddScoped<CurrentSession>(x =>
+            {
+                return new CurrentSession
+                {
+                    Environment = _environment.EnvironmentName == nameof(DefinedEnvironments.Development) ?
+                                    DefinedEnvironments.Development :
+                                    DefinedEnvironments.Production
+                };
+            });
+
+            _services.AddControllers().AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+            });
+            _services.AddScoped<IHtmlToPdfConverter, HtmlToPdfConverter>();
+            _services.AddScoped<IHtmlToDocxConverter, HtmlToDocxConverter>();
+            _services.AddScoped<IDataTableToExcel, DataTableToExcel>();
+            _services.AddScoped<IDataTableToExcel, DataTableToExcel>();
+            _services.AddSingleton(typeof(IConverter), new SynchronizedConverter(new PdfTools()));
+            _services.AddScoped<IHTMLConverter, HTMLConverter>();
+
+            _services.AddCors(options =>
+            {
+                options.AddPolicy("bottomhalf-cors", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .WithExposedHeaders("Authorization");
+                });
+            });
+        }
+    }
+
+    public class CustomAssemblyLoadContext : AssemblyLoadContext
+    {
+        public IntPtr LoadUnmanagedLibrary(string unmanagedLibraryName)
+        {
+            return LoadUnmanagedDll(unmanagedLibraryName);
+        }
+
+        protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "lib", $"{unmanagedDllName}.dll");
+            return LoadUnmanagedDllFromPath(path);
+        }
+    }
+}
